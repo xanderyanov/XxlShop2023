76673fa25e34a4a50bd31bc404d76569bda2a511
xanderyanov <xanderyanov@gmail.com>
2023-03-24 14:14:43 +0400

    1

---
+++ XxlStore/Models/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Runtime.Internal.Endpoints.StandardLibrary;
+
+namespace XxlStore.Models
+{
+    public class MenuItem
+    {
+        public MenuItem Parent;
+
+        public string Url;
+        public MenuItem(string url)
+        {
+            Url = url;
+        }
+
+        public bool IsSelected;
+
+        public List<MenuItem> Children = new();
+
+        
+    }
+}
